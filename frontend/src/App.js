6a66b7a6c8a68670723ea2498a2e06979cc0d2eb
RChakradharii <160701886+RChakradharii@users.noreply.github.com>
2024-12-21 16:03:49 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
 useEffect(() => {
   const fetchTasks = async () => {
     try {
-      const response = await axios.get("https://task-manager-4-juln.onrender.com/tasks"); // Update this URL
+      const response = await axios.get("https://task-manager-4-juln.onrender.com"); // Update this URL
       setTasks(response.data);
     } catch (error) {
       console.error("Error fetching tasks:", error);
