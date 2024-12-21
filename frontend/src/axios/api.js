41a38bb43516b6e12828e4e03c43561beddb52bf
RChakradharii <160701886+RChakradharii@users.noreply.github.com>
2024-12-21 11:28:05 +0530

    Update api.js

--- frontend/src/axios/api.js
+++ frontend/src/axios/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000", // Backend base URL
+  baseURL: "https://task-manager-4-juln.onrender.com", // Backend base URL
 });
 
 export default instance;
